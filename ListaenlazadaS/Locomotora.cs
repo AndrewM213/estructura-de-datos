5d689dea009dc1dddcb2ee48ca88adca61f5947f
AndrewM213 <andrew211104@gmai.com>
2025-03-27 17:42:24 -0500

    vagones

---
+++ ListaenlazadaS/Locomotora.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaEnlazadaS
+{
+    class Locomotora
+    {
+        Vagon Primero {  get; set; }
+        public void AgregarPrimero(int valor)
+        {
+            Vagon nuevo = new Vagon(valor);
+
+            if (Primero == null)
+            {
+                this.Primero = nuevo;
+            }
+            else
+            {
+                nuevo.Sig = this.Primero;
+                this.Primero = nuevo;
+                
+            }
+
+        }
+        
+        public void AgregaFinal(int valor)
+        {
+            Vagon nuevo = new Vagon(valor);
+    
+            if (Primero == null)
+            {
+                this.Primero = nuevo;
+            }
+            else
+            {
+                Vagon tmp = this.Primero;
+                while (tmp.Sig != null)
+                {
+                    tmp = tmp.Sig;
+                }
+                tmp.Sig = nuevo;
+            }
+
+
+        }
+        public string VerVagones()
+        {
+            string ListaVag = "";
+            Vagon tmp = this.Primero;
+            while (tmp != null)
+            {
+                ListaVag += tmp.Dato + ", ";
+                tmp = tmp.Sig;
+            }
+            return ListaVag;
+        }
+    }
+
+}
--- ListaenlazadaS/Program.cs
+++ ListaenlazadaS/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ListaEnlazadaS;
+
+string ListaEnlazadaS;
+
+Locomotora l = new Locomotora();
+l.AgregaFinal(1);
+l.AgregaFinal(3);
+l.AgregaFinal(34);
+Console.WriteLine(l.VerVagones());
